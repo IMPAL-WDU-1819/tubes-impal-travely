8d46d2744f393972184e2fed35c9dff024d1ddea
hanafigusman97 <hanafiabdullah97@gmail.com>
2018-09-13 15:40:25 +0700

    Update ddl, insert beberapa data

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 13, 2018 at 01:27 AM
--- Server version: 5.7.21
--- PHP Version: 7.1.16
+-- Generation Time: Sep 13, 2018 at 10:34 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -38,6 +36,16 @@ CREATE TABLE `akun` (
   `alamat` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `akun`
+--
+
+INSERT INTO `akun` (`id_akun`, `email`, `nama`, `password`, `tanggal_lahir`, `nomor_telepon`, `alamat`) VALUES
+('123', 'hanafigusman@hotmail.com', 'Hanafi Abdullah Gusman', 'taiayam', '09-08-97', '08999457754', 'PGA EL FARINA'),
+('124', 'refinheryanto@gmail.com', 'Refin Heryanto', 'taiayam', '09-09-98', '08999458854', 'PGA EL FARINA'),
+('125', 'fajaralif92@gmail.com', 'Muchammad Fajar alif', 'taiayam', '09-10-98', '08999459954', 'kost elfarina PGA NO 44'),
+('126', 'taufiqdawami@gmail.com', 'Taufiq Akmal Dawami', 'taiayam', '09-11-98', '08999333333', 'gang ummayah');
+
 -- --------------------------------------------------------
 
 --
@@ -48,6 +56,15 @@ CREATE TABLE `domisili` (
   `id_domisili` varchar(3) NOT NULL,
   `nama_domisili` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `domisili`
+--
+
+INSERT INTO `domisili` (`id_domisili`, `nama_domisili`) VALUES
+('BDG', 'Bandung'),
+('JKT', 'DKI Jakarta'),
+('TGS', 'Tangerang Selatan');
 
 -- --------------------------------------------------------
 
@@ -66,6 +83,14 @@ CREATE TABLE `jadwal` (
   `kereta` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `jadwal`
+--
+
+INSERT INTO `jadwal` (`id_jadwal`, `tanggal_berangkat`, `tanggal_tiba`, `stasiun_asal`, `stasiun_tujuan`, `waktu_berangkat`, `waktu_sampai`, `kereta`) VALUES
+('100', '18-01-2018', '18-01-2018', 'KRC', 'STB', '09.00', '13.00', 'PTS13'),
+('101', '20-02-2018', '21-02-2018', 'KRC', 'PPJ', '15.00', '18.00', 'BM12');
+
 -- --------------------------------------------------------
 
 --
@@ -76,6 +101,15 @@ CREATE TABLE `kelas` (
   `tipe` varchar(5) NOT NULL,
   `harga` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`tipe`, `harga`) VALUES
+('REG', 50000),
+('VIP', 100000),
+('VVIP', 500000);
 
 -- --------------------------------------------------------
 
@@ -88,6 +122,16 @@ CREATE TABLE `kereta` (
   `nama_kereta` varchar(20) NOT NULL,
   `kapasitas` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `kereta`
+--
+
+INSERT INTO `kereta` (`id_kereta`, `nama_kereta`, `kapasitas`) VALUES
+('BM12', 'BIMA', 200),
+('KCN20', 'KENCANA', 100),
+('KEO40', 'KREO', 250),
+('PTS13', 'PATAS AC', 300);
 
 -- --------------------------------------------------------
 
@@ -102,6 +146,16 @@ CREATE TABLE `kursi` (
   `kelas` varchar(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `kursi`
+--
+
+INSERT INTO `kursi` (`id_kursi`, `no_kursi`, `kereta`, `kelas`) VALUES
+('A1', '1', 'BM12', 'VIP'),
+('B1', '1', 'KCN20', 'REG'),
+('C1', '1', 'KEO40', 'REG'),
+('D1', '1', 'PTS13', 'VIP');
+
 -- --------------------------------------------------------
 
 --
@@ -113,6 +167,16 @@ CREATE TABLE `stasiun` (
   `nama_stasiun` varchar(20) NOT NULL,
   `domisili` varchar(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `stasiun`
+--
+
+INSERT INTO `stasiun` (`id_stasiun`, `nama_stasiun`, `domisili`) VALUES
+('GBR', 'Gambir', 'JKT'),
+('KRC', 'Kiara Condong', 'BDG'),
+('PPJ', 'Pondok Ranji', 'TGS'),
+('STB', 'Bandung', 'BDG');
 
 -- --------------------------------------------------------
 
@@ -203,7 +267,6 @@ ALTER TABLE `tiket`
   ADD PRIMARY KEY (`id_tiket`),
   ADD KEY `kursi` (`kursi`),
   ADD KEY `jadwal` (`jadwal`),
-  ADD KEY `transkasi` (`transkasi`);
 
 --
 -- Indexes for table `transaksi`
@@ -241,9 +304,7 @@ ALTER TABLE `stasiun`
 --
 ALTER TABLE `tiket`
   ADD CONSTRAINT `tiket_ibfk_1` FOREIGN KEY (`kursi`) REFERENCES `kursi` (`id_kursi`),
-  ADD CONSTRAINT `tiket_ibfk_2` FOREIGN KEY (`jadwal`) REFERENCES `jadwal` (`id_jadwal`),
-  ADD CONSTRAINT `tiket_ibfk_3` FOREIGN KEY (`transkasi`) REFERENCES `transaksi` (`id_transaksi`);
-COMMIT;
+  ADD CONSTRAINT `tiket_ibfk_2` FOREIGN KEY (`jadwal`) REFERENCES `jadwal` (`id_jadwal`)
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
